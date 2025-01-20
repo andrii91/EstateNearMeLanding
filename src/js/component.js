0832e61916ca254a2bf321cd96845d983b15fb04
Andriy Lytvyn <andriy@devsalsa.com>
2025-01-20 22:14:28 +0200

    add modals first screen

--- src/js/component.js
+++ src/js/component.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
   /* form valid*/
-  let alertImage = '<svg class="absolute right-6 bottom-5 w-5 h-5 text-rose-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="currentColor"/></svg>';
+  let alertImage = '<svg class="svg-valid" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="currentColor"/></svg>';
   let error;
   $('.submit').click(function (e) {
     e.preventDefault();
@@ -9,40 +9,47 @@ $( document ).ready(function() {
     $(ref).each(function () {
       let thisFiled = $(this);
 
-      if ($(this).val().trim() === '') {
-          thisFiled.addClass('error').parent('.label').append('<div class="allert">' + alertImage + '</div>');
+      if (thisFiled.val().trim() === '') {
+        thisFiled.removeClass('error').parents('.label').find('.allert').remove();
+        const errorMessage = 'Enter value this filed';
+          thisFiled.addClass('error').parents('.label').append(`<div class="allert">${alertImage} ${errorMessage}</div>`);
           error = 1;
           $(":input.error:first").focus();
           return false;
       } else {
         if (thisFiled.attr("type") === 'email') {
-          let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          if (!pattern.test(thisFiled.val())) {
-            $("input[name=email]").val('');
-            thisFiled.addClass('error').parent('.label').append('<div class="allert">' + alertImage + '</div>');
+          thisFiled.removeClass('error').parents('.label').find('.allert').remove();
+          emailValue = thisFiled.val();
+          const errorMessage = validateEmail(emailValue);
+          if (errorMessage) {
+            thisFiled.val('');
+            thisFiled.addClass('error').parents('.label').append(`<div class="allert">${alertImage} ${errorMessage}</div>`);
             error = 1;
             $(":input.error:first").focus();
             return false;
           }else{
             error = 0;
-            thisFiled.removeClass('error').parent('.label').find('.allert').remove();
+            thisFiled.removeClass('error').parents('.label').find('.allert').remove();
           }
         } else if (thisFiled.attr("type") === 'tel') {
-          let patterntel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+          thisFiled.removeClass('error').parents('.label').find('.allert').remove();
+          let patterntel = /^[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+          const errorMessage = 'Enter valid phone';
           if (!patterntel.test(thisFiled.val())) {
-            $("input[name=phone]").val('');
-            thisFiled.addClass('error').parent('.label').append('<div class="allert">' + alertImage + '</div>');
+            thisFiled.val('');
+            thisFiled.addClass('error').parents('.label').append(`<div class="allert">${alertImage} ${errorMessage}</div>`);
             error = 1;
             $(":input.error:first").focus();
             return false;
           }else{
             error = 0;
-            thisFiled.removeClass('error').parent('.label').find('.allert').remove();
+            thisFiled.removeClass('error').parents('.label').find('.allert').remove();
           }
 
         }  else {
           error = 0;
-          thisFiled.removeClass('error').parent('.label').find('.allert').remove();
+          thisFiled.removeClass('error').parents('.label').find('.allert').remove();
         }
       }
     });
@@ -51,7 +58,7 @@ $( document ).ready(function() {
     }
   });
 
-  $('form').on('submit', function (e) {
+  $('.get-price-form').on('submit', function (e) {
     e.preventDefault();
     var $form = $(this);
     $form.find('.submit').addClass('inactive');
@@ -65,14 +72,92 @@ $( document ).ready(function() {
       $form.find('.submit').prop('disabled', false);
       $form[0].reset();
 
-      $('#check-success').prop('disabled', true)
+      $('.tab-item').removeClass('active')
+      $('#slide-5').addClass('active');
+
       
     }, 1000);
 
   });
 
+  const ofertaValue = $('#oferta');
+  if(ofertaValue.is(':checked')) {
+    $('.get-price-form-submit').removeAttr('disabled')
+  }else{
+    $('.get-price-form-submit').attr('disabled', true)
+  }
 
-  $('input[name="phone"]').inputmask("{1,15}");
+  ofertaValue.change(function(){
+    if(ofertaValue.is(':checked')) {
+      $('.get-price-form-submit').removeAttr('disabled')
+    }else{
+      $('.get-price-form-submit').attr('disabled', true)
+    }
+  })
+
+
+  $('#call-me-form').on('submit', function (e) {
+    e.preventDefault();
+    var $form = $(this);
+    $form.find('.submit').addClass('inactive');
+    $form.find('.submit').prop('disabled', true);
+
+
+    setTimeout(function () {
+      alert('Success');
+      
+      $form.find('.submit').removeClass('inactive');
+      $form.find('.submit').prop('disabled', false);
+      $form[0].reset();
+      
+    }, 1000);
+
+  });
+
+  function validateEmail(email) {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+    if (!email.includes('@')) {
+      return "Invalid email format: '@' symbol is missing.";
+    }
+  
+    const parts = email.split('@');
+    if (parts.length !== 2) {
+      return "Invalid email format: Too many '@' symbols.";
+    }
+  
+    const [localPart, domain] = parts;
+    if (!localPart) {
+      return "Invalid email format: Missing local part before '@'.";
+    }
+  
+    if (!domain || !domain.includes('.')) {
+      return "Invalid email format: Domain is incomplete.";
+    }
+  
+    if (!re.test(email)) {
+      return "Invalid email format: Email does not meet the standard format.";
+    }
+  
+    return null; 
+  }
+
+  const ofertaValue1 = $('#oferta_1');
+  if(ofertaValue1.is(':checked')) {
+    $('.call-me-submit').removeAttr('disabled')
+  }else{
+    $('.call-me-submit').attr('disabled', true)
+  }
+
+  ofertaValue1.change(function(){
+    if(ofertaValue1.is(':checked')) {
+      $('.call-me-submit').removeAttr('disabled')
+    }else{
+      $('.call-me-submit').attr('disabled', true)
+    }
+  })
+
+  // $('input[name="phone"]').inputmask("{1,15}");
 
 
   // $(window).scroll(function () {
@@ -145,9 +230,6 @@ $( document ).ready(function() {
   $(document).keydown(function (e) {
     // ESCAPE key pressed 
     if (e.keyCode == 27) {
-      if ($('.modal.show').attr('id') === 'media') {
-        $('#media').find('.modal-content').text('')
-      }
       $('.modal').removeClass('show');
       $('body').removeClass('overflow-hidden');
     }
@@ -155,13 +237,22 @@ $( document ).ready(function() {
 
 
   $('[data-tab]').click(function (e) {
-    e.preventDefault();
 
     const $this = $(this),
-      tab = $this.data('tab') ?? $this.attr('href');
+      tab = $this.data('tab');
 
     $('.tab-item').removeClass('active')
     $(tab).addClass('active');
+  })
+
+  $('.go-to-home-btn').click(function(){
+    $('.modal').removeClass('show');
+    $('body').removeClass('overflow-hidden');
+  })
+
+  $('.mobile-btn').click(function(){
+    $(this).toggleClass('active');
+    $('.navigation-menu, .mobile-phone').toggleClass('show');
   })
 
 
